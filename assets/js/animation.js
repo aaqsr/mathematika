724fcc1dbcdcc5a518eddb2b04c38278c56f0729
Awab Qureshi <awab.qureshi@gmail.com>
2021-06-16 23:30:57 +0500

    Added multiple quotes to animation

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,10 +1,27 @@
-new TypeIt("#typed", {
-    strings: [
-        '"Pure Mathematics is, in its way, the <em>Poetry</em> of logical ideas" ~Einstein',
-    ],
+let typer = new TypeIt("#typedQuotes", {
     html: true,
     speed:  80,
-    loop: true,
+    loop: false,
     loopDelay: 4000,
-    cursorChar: '<span class="green-text">|</span>'
-}).go();
+    cursorChar: '<span style="color: lime">|</span>',
+    waitUntilVisible: true,
+})
+
+var content = [
+    '"Pure Mathematics is, in its way, the <em>Poetry</em> of logical ideas" ~Einstein',
+    '"Logic is the foundation of the certainty of all the knowledge we acquire." ~Euler',
+    '"The enchanting charms of this <em>sublime science</em> reveal only to those who have the <em>courage</em> to go deeply into it." ~Gauss',
+    '"Mathematics is the most beautiful and most powerful creation of the human spirit" ~Banach',
+    '"There should be no such thing as boring mathematics." ~Edsger',
+    '"If only I had the Theorems! Then I should find the proofs easily enough." ~Riemann',
+    '"It is impossible to be a mathematician without being a poet in soul." ~Sofia Kovalevskaya"',
+];
+
+var p = 1500;
+
+for(const str of content) {
+  typer.type(str).pause(p).delete(0 - str.length);
+}
+
+typer.go();
+
