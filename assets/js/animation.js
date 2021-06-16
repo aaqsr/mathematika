b9d8937bf1292b62ceefe510af2f38f74051f689
Awab <Awab@WS183>
2021-06-16 17:28:33 +0500

    Changed animation

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,5 +1,10 @@
 new TypeIt("#typed", {
-    strings: "\"Pure Mathematics is, in its way, the Poetry of logical ideas\"",
-    speed: 100,
-    loop: true
+    strings: [
+        '"Pure Mathematics is, in its way, the <em>Poetry</em> of logical ideas" ~Einstein',
+    ],
+    html: true,
+    speed:  80,
+    loop: true,
+    loopDelay: 4000,
+    cursorChar: '<span class="green-text">|</span>'
 }).go();
